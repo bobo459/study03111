fbd28777b32452226af40e5a432930e65a63f4fa
iingkejin <ingkejin@gamil.com>
2024-03-14 17:43:30 +0900

    V0314

--- src/Test/test20240312.java
+++ src/Test/test20240312.java
@@ -1,3 +1,5 @@
+package Test;
+
 public class test20240312 {
     public static void main(String[] args) {
 
--- src/Test/test30304.java
+++ src/Test/test30304.java
@@ -1,3 +1,5 @@
+package Test;
+
 public class test30304 {
     public static void main(String[] args) {
 
