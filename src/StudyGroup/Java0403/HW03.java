cbe3f6e3bad9ea7f1c49e7330f5d794b498428ab
iingkejin <ingkejin@gamil.com>
2024-04-04 22:04:00 +0900

    v0404

--- src/StudyGroup/Java0403/HW03.java
+++ src/StudyGroup/Java0403/HW03.java
@@ -1,15 +1,54 @@
 package StudyGroup.Java0403;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-//st1, st2,st3 3명의 학생의 성적을 입력받아 배열에 저장한다.
+//st1, st2,st3 3명의 학생의 성적을 입력받아 배열에 저장한다.  <==
 //그 학생들의 평균성적을 구한 뒤 구한 평균 성적을 바탕으로 학생들의 성적을 분류하여 출력한다.
 // 결과값 예시 => 학생명 : st1, 점수 : 100점, 평균 이상(혹은 이하)
 //*이하는 기준점을 포함한 그보다 아랫수자를 의미함*
 public class HW03 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("");
+        System.out.print("학생 st1 의 성적을 적어주세요. : ");
+        int Ouput = sc.nextInt();
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("학생 st2 의 성적을 적어주세요. : ");
+        int Ouput1 = sc1.nextInt();
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.print("학생 st3 의 성적을 적어주세요. : ");
+        int Ouput2 = sc2.nextInt();
+
+
+        int sum = 0;
+        int mul = 0;
+        int B = 0;
+
+        int[] Array = new int[3];
+        Array[0]=Ouput;
+        Array[1]=Ouput1;
+        Array[2]=Ouput2;
+
+        for (int i = 0; i<Array.length;i++){
+            sum = sum+ Array[i];
+            mul = sum/Array.length;
+
+        } System.out.println(Arrays.toString(Array));
+        System.out.println(sum);
+        System.out.println(mul);
+
+        for (int j = 0; j<Array.length; j++){
+
+            if (Array[j] >= mul){
+                System.out.println("학생명 : " + "점수 : " +  "평균 이상");
+
+            }else {
+                System.out.println("학생명 : " + "점수 : " +  "평균 이하");
+            }
+        }
 
     }
 }
---
+++ src/StudyGroup/Java0404/EX01.java
@@ -0,0 +1,57 @@
+package StudyGroup.Java0404;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+//1~100까지의 숫자 중 5의 배수인 숫자는 Array5 에 저장하고 [20개] <=
+//10의 배수이 숫자는 Array 10에 저장한다.  [10개]  <=
+//그리고
+//1. Array5 의 인덱스의 총 합을 sum5 에 저장한다. 출력   <=
+//2. Array5와 Array10의 공통적으로 들어가는 숫자(공배수)를 Array0 이라는 배열에 저장하시오. 출력
+public class EX01 {
+    public static void main(String[] args) {
+        int a= 0;
+        int b= 0;
+        int c= 0;
+        int sum5= 0;
+
+        int[] Array5 = new int[20];
+        int[] Array10 = new int[10];
+        int[] Array0 = new int[10];
+
+        for (int i =1;i<=100;i++){
+            if ( i % 5 == 0) {
+                Array5[a] = i;
+                a++;
+            }
+        }
+        for (int k=0;k<Array5.length;k++){
+            sum5 = sum5 + Array5[k];
+        }
+        System.out.println("총 합은 "+sum5);
+
+        for (int j =1;j<=100;j++){
+            if ( j % 10 == 0) {
+                Array10[b] = j;
+                b++;
+            }
+        }
+/*
+        for (int m=0;m<Array5.length;m++){
+            if (Array5[a]==Array10[b]){
+                Array0[c] = m;
+            }//System.out.println(m);
+        }//System.out.println(Arrays.toString(Array0));
+*/
+        System.out.println(Arrays.toString(Array5));
+        System.out.println(Arrays.toString(Array10));
+        for (int i=0;i<100;i++){
+            if (Array5==Array10){
+                Array0[c]= i;
+                i++;
+            }
+        }System.out.println(Arrays.toString(Array0));
+
+    }
+}
