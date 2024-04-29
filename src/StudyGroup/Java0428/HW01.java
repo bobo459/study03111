37ef684efb73f67bec76dc82d50cc7b451adad7d
bobo459 <awd458@naver.com>
2024-04-29 12:50:33 +0900

    v0429

--- src/StudyGroup/Java0428/HW01.java
+++ src/StudyGroup/Java0428/HW01.java
@@ -4,23 +4,21 @@ package StudyGroup.Java0428;
 //2. 반의 전체 평균을 구하시오.
 //3. 학생 5명에 대한 국어 평균 이상 이하 학생을 구분하시오.
 //4. 모든 학생의 각 과목별 총점을 담고 있는 객체를 출력하시오.
-
-
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class HW01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int man =5;
 
-        int[] a = new  int[4];
-        int[] b = new  int[4];
-        int[] c = new  int[4];
-        int[] d = new  int[4];
-        int[] e = new  int[4];
+        int[] a = new  int[man];
+        int[] b = new  int[man];
+        int[] c = new  int[man];
+        int[] d = new  int[man];
+        int[] e = new  int[man];
         int i;
-        float avg = 0;
-        int sum = 0;
+        float[] avg = new float[man];
+        int[] sum = new int [man];
         System.out.print("1번학생 국어, 수학, 사회, 과학 점수를 적어주세요 : ");
         for (i = 0; i < a.length; i++) {
             int age = sc.nextInt();
@@ -43,13 +41,18 @@ public class HW01 {
             int age4 = sc.nextInt();
             e[i] =  age4;
         }
-      //  sum = sum +age;
-        avg = sum / a.length;
-        System.out.println(Arrays.toString(a));
-        System.out.println(Arrays.toString(b));
-        System.out.println(Arrays.toString(c));
-        System.out.println(Arrays.toString(d));
-        System.out.println(Arrays.toString(e));
-      //  System.out.println(avg);
+        //1. 각 과목별로 평균 점수를 구하시오.
+        for (int j = 0; j < man; j++) {
+            sum[j] = a[j]+b[j]+c[j]+d[j]+e[j];
+            avg[j] = sum[j] /man;
+        }
+        System.out.println("1학생의 성적 : "+ Arrays.toString(a));
+        System.out.println("2학생의 성적 : "+Arrays.toString(b));
+        System.out.println("3학생의 성적 : "+Arrays.toString(c));
+        System.out.println("4학생의 성적 : "+Arrays.toString(d));
+        System.out.println("5학생의 성적 : "+Arrays.toString(e));
+        System.out.println("전체 과목별 성적 합계 : "+Arrays.toString(sum));
+        System.out.println("전체 과목별 평균 : "+Arrays.toString(avg));
+
     }
 }
